3d5aa1aa2c06add782beac1ac6dcf27298868426
Mitchell Osborn <39538422+mitosborn@users.noreply.github.com>
2021-01-21 15:10:47 -0800

    Resolved Data storage
    Data is now stored locally on the browser, preventing issues with multiple instances impacting one another.

--- app.py
+++ app.py
@@ -100,21 +100,21 @@ row2 = dbc.Row(dbc.Col([html.H3("Generate a New Playlist"),row2_form],width = 3)
 content = html.Div([dbc.Row([dbc.Col(dcc.Graph(id="playlist_graph"),width = 6,style = {'background-color':'red'}),dbc.Col(dcc.Graph(id = "playlist_graph2"),width = 6)],style = {'background-color':'blue'}),row2], style = CONTENT_STYLE)
 #content = html.Div(id = 'page-content', style = CONTENT_STYLE)
 
-data = html.Div(id='intermediate-value', style={'display': 'none'})
+data = dcc.Store(id='playlist_data')
+playlist_ids = dcc.Store(id='playlist_ids')
+app.layout = html.Div([sidebar,content,data,playlist_ids])
 
-app.layout = html.Div([sidebar,content,data])
 
 
-
-def make_graph(items):
+def make_graph(items, data):
     #global data
     # print("Valid data"+ str(valid_data))
     # print("Current ids" + str(current_ids))
     filter = [x for x in items if (x is not None and x != "")]
-    if len(filter) > 0:
-        data = PlaylistRetriever.get_user_playlists(filter)
-    else:
-        data = PlaylistRetriever.get_user_playlists(current_ids)
+    # if len(filter) > 0:
+    #     data = PlaylistRetriever.get_user_playlists(filter)
+    # else:
+    #     data = PlaylistRetriever.get_user_playlists(current_ids)
     print("got here")
 
     fig = Visualization.make_radar_chart(data)
@@ -135,21 +135,20 @@ style_todo = {"display": "inline", "margin": "10px"}
 style_done = {"textDecoration": "line-through", "color": "#888"}
 style_done.update(style_todo)
 
-@app.callback(Input("list-container", "children"),Output('intermediate-value','children')
-)
-def get_data(children):
-    pass
-
-
+@app.callback(Output('playlist_data','data'),Input('playlist_ids', 'data'))
+def get_data(id_tup):
+    new_ids = [x[0] for x in id_tup[0]]
+    print("Inside get data")
+    print(new_ids)
+    if new_ids:
+        data = PlaylistRetriever.get_user_playlists(new_ids)
+    else:
+        data = PlaylistRetriever.get_user_playlists(current_ids)
+    #print(data)
+    return data.reset_index().to_json()
 
 @app.callback(
-    [
-        Output("list-container", "children"),
-        Output("new-item", "value"),
-        Output("playlist_graph", "figure"),
-        Output("playlist_graph2","figure"),
-
-    ],
+        Output('playlist_ids','data'),
     [
         Input("add", "n_clicks"),
         Input("new-item", "n_submit"),
@@ -161,9 +160,9 @@ def get_data(children):
         State({"index": ALL, "type": "done"}, "value")
     ]
 )
-def edit_list(add, add2, clear, new_item, items, items_done):
-    print(add,add2,clear)
-    print(new_item,items,items_done)
+def get_list_content(add, add2, clear, new_item, items, items_done):
+    print(add, add2, clear)
+    print(new_item, items, items_done)
     triggered = [t["prop_id"] for t in dash.callback_context.triggered]
     adding = len([1 for i in triggered if i in ("add.n_clicks", "new-item.n_submit")])
     clearing = len([1 for i in triggered if i == "clear-done.n_clicks"])
@@ -175,6 +174,34 @@ def edit_list(add, add2, clear, new_item, items, items_done):
     if adding:
         new_spec.append((new_item, []))
     print("new spec:" + str(new_spec))
+    return [new_spec, "" if adding else new_item]
+
+
+@app.callback(
+    [
+        Output("list-container", "children"),
+        Output("new-item", "value"),
+        Output("playlist_graph", "figure"),
+        Output("playlist_graph2", "figure")
+    ],
+    [
+        # Input("add", "n_clicks"),
+        # Input("new-item", "n_submit"),
+        # Input("clear-done", "n_clicks"),
+        Input('playlist_data','data'),
+        Input('playlist_ids','data')
+    ]
+    # ,
+    # [
+    #     State("new-item", "value"),
+    #     State({"index": ALL}, "children"),
+    #     State({"index": ALL, "type": "done"}, "value")
+    # ]
+)
+def edit_list(dataframe, playlist_ids):
+    dataframe = pd.read_json(dataframe)
+    adding  = playlist_ids[1]
+    new_spec = playlist_ids[0]
     new_list = [
         html.Div([
             dcc.Checklist(
@@ -188,8 +215,8 @@ def edit_list(add, add2, clear, new_item, items, items_done):
         ], style={"clear": "both"})
         for i, (text, done) in enumerate(new_spec)
     ]
-    graph = make_graph([x[0] for x in new_spec])
-    y=  [new_list, "" if adding else new_item,graph,graph,html.Div()]
+    graph = make_graph(new_spec,dataframe)
+    y=  [new_list, adding, graph, graph]
     print(y)
     return y
 
